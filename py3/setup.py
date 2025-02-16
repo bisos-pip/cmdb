9f42628c408e11606838b04dcd4b94034387301f
Mohsen Banan <form@mohsen.banan.1.byname.net>
2025-02-16 12:25:11 -0800

    0.13 un-unisos and un-blee.

--- py3/setup.py
+++ py3/setup.py
@@ -43,7 +43,7 @@ def longDescription():
 # b:py3:pypi:setup/version Arguments  :forSys t :forPyPi t :constant "666"
 ####+BEGIN: b:py3:pypi:setup/version :comment "Auto Detected"
 
-# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.12 -- forSysVersion=0.10 -- constant=NA
+# ./pypiUploadVer DID NOT exist -- forPypiVersion=0.13 -- forLocalVersion=0.10 -- constant=NA
 def pkgVersion():
         return '0.10'
 
@@ -53,8 +53,6 @@ def pkgVersion():
 ####+BEGIN: b:py3:pypi:setup/requires :extras ()
 
 requires = [
-"blee",
-"bisos.csPlayer",
 "bisos",
 "bisos.b",
 "bisos.basics",
